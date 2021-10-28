0a0dc473df59aeb5632106c8e3c0e066299af80d
rafaelMartins1922 <rafael@ejcm.com.br>
2021-10-28 13:32:42 -0300

    adding routes to router

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,1 +1,10 @@
+const { Router } = require('express');
+const IpController = require('../controllers/IpController');
 
+const router = Router();
+
+router.get('/ips/index', IpController.index);
+router.get('/ips/nonblacklisted', IpController.nonBlacklistedIpsIndex);
+router.post('/ips/blacklist', IpController.blacklistIp);
+
+module.exports = router;
