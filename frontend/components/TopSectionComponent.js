1360fd70224970d5d3803985cc6b846330792b6c
rafaelMartins1922 <rafael@ejcm.com.br>
2021-11-04 17:37:00 -0300

    Changing main page text

--- frontend/components/TopSectionComponent.js
+++ frontend/components/TopSectionComponent.js
@@ -6,7 +6,7 @@ export default function TopSectionComponent() {
             <div className="top-section-bg"></div>
             <div className="intro-text">
                 <h1 className="headline"> IP Blacklist Checker</h1>
-                <p className="instructions">Utilize a lista abaixo para verificar os IPs seguros e não seguros da rede Tor. <br/>
+                <p className="instructions">Utilize a lista abaixo para verificar os IPs da rede Tor. <br/>
                 Clique no ícone de bloqueio para adicionar um IP à lista de bloqueados.
                 </p>
             </div>
