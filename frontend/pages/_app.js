ae5fbf5daa177efaa1a7df36946728359ccbd268
rafaelMartins1922 <rafael@ejcm.com.br>
2021-11-04 17:23:12 -0300

    Removing global context in front end's _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,8 @@
 import '../styles/globals.css'
-import { AppWrapper } from '../context/globalState'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AppWrapper>
       <Component {...pageProps} />
-    </AppWrapper>
   )
 }
 
